8c4a4601c4c98b07f526fa4fc49f63d423124d47
fananchong <fananchong@126.com>
2018-12-24 22:19:57 +0800

    go vendor 升级为 go modules

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 set -ex
 
-docker run --rm -e GOBIN=/go/bin/ -v "$PWD"/bin:/go/bin/ -v "$PWD":/go/src/github.com/fananchong/goredis -w /go/src/github.com/fananchong/goredis golang go install ./...
+export GOBIN=$PWD/bin
+go install ./...
---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/fananchong/goredis
+
+require (
+	github.com/FZambia/sentinel v1.0.0
+	github.com/gomodule/redigo v2.0.0+incompatible
+	github.com/mna/redisc v1.1.3
+)
